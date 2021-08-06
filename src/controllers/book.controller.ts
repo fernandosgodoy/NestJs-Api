5e1ba431c217442fad9516e649491fb99da0dc37
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-08-06 20:24:04 -0300

    created the controller for Books operations

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,11 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import Book from 'src/model/book';
+import BookService from 'src/services/bookService';
 
 @Controller()
 export default class BookController {
 
+    constructor(private bookService: BookService) {
+    }
+
     @Get()
-    public GetAll() {
-        
+    public ListAll(): Book[] {
+        return this.bookService.getAll();
+    }
+
+    @Get(':id')
+    public GetSingle(@Param() params): Book {
+        return this.bookService.getById(params.id);
+    }
+    
+    @Post()
+    public Create(book: Book) {
+        this.bookService.add(book);
+    }
+
+    @Put()
+    public Update(book: Book) {
+        this.bookService.update(book);
+    }
+
+    @Delete(':id')
+    public Remove(@Param() params) {
+        this.bookService.delete(params.id);
     }
 
 }
