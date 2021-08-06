2f3433c01c62d3ea5cdd6e342cbc9cee07d83c30
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-08-06 20:29:26 -0300

    included the sequelize packages and the connection to mysql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import BookController from './controllers/book.controller';
+import Book from './model/book';
+import BookService from './services/bookService';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123',
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    SequelizeModule.forFeature([Book])
+  ],
+  controllers: [AppController, BookController],
+  providers: [AppService, BookService],
 })
 export class AppModule {}
